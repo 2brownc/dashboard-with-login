73357c1ad9f64a7c9c28f82c0cb7c6af48b3b818
brown <86018533+2brownc@users.noreply.github.com>
2022-08-02 06:17:05 +0530

    fade in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Fade from '@mui/material/Fade';
+import Box from '@mui/material/Box';
 
 import LoginPage from './components/account/Login';
 import SignUpPage from './components/account/SignUp';
-
 import DashboardMain from './components/dashboard/Main';
 
 function App() {
+  // fade in the login form
+  const [show, setShow] = React.useState(false);
+  React.useEffect(() => {
+    setTimeout(() => { setShow(true) }, 500)
+  }, []);
+
   return (
-    <DashboardMain />
+    <Fade in={show}>
+      <Box>
+        <LoginPage />
+      </Box>
+    </Fade>
   );
 }
 
