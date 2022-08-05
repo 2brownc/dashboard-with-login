69e8dd7fd86ffec4fff0634ffe009ce225b3c8b0
brown <86018533+2brownc@users.noreply.github.com>
2022-08-05 15:34:55 +0530

    set up auth

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,43 +1,87 @@
-import passwordHash from 'pbkdf2-password-hash'
+import SHA512 from 'sha512-es';
 
-function userSignUp(username, email, password) {
-  passwordHash.hash(password)
-    .then((hash) => {
-      localStorage.setItem(
-        username,
-        {
-          passwordHash: hash,
-          email: email,
-        }
-      );
-    })
+function getHash(key) {
+  return SHA512.hash(key);
+}
+
+function compareHash(key, hash) {
+  const hashFromKey = getHash(key);
+  return hashFromKey === hash;
 }
 
 function userLogin(username, password) {
   const result = {
-    username: username,
+    username,
     email: null,
+    firstname: null,
+    lastname: null,
     accountExists: null,
     correctPassword: null,
   };
-  const storedData = localStorage.getItem(username);
+  const storedData = JSON.parse(localStorage.getItem(username));
 
   if (!storedData) {
     result.accountExists = false;
   } else {
     result.accountExists = true;
-    passwordHash.compare(password, storedData.passwordHash)
-      .then((isValid) => {
-        if (isValid === true) {
-          result.email = storedData.email;
-          result.correctPassword = false;
-        } else {
-          result.correctPassword = true;
-        }
-      })
+    if (compareHash(password, storedData.passwordHash) === true) {
+      result.email = storedData.email;
+      result.correctPassword = true;
+      result.firstname = storedData.firstname;
+      result.lastname = storedData.lastname;
+    } else {
+      result.correctPassword = false;
+    }
   }
-
   return result;
 }
 
-export { userSignUp, userLogin };
+function userSignUp(firstname, lastname, username, email, password) {
+  try {
+    const passwordHash = getHash(password);
+
+    const accountData = {
+      passwordHash,
+      email,
+      username,
+      firstname,
+      lastname,
+    };
+
+    localStorage.setItem(username, JSON.stringify(accountData));
+
+    return userLogin(username, password);
+  } catch (exception) {
+    // account creation failed
+    return null;
+  }
+}
+
+function isNewUser(username) {
+  const storedData = localStorage.getItem(username);
+
+  if (!storedData) {
+    /*
+    if no such username is found
+    return true
+    meaning no such user exits
+    */
+    return true;
+  }
+  return false;
+}
+
+// Create a DEMO USER
+function createDemoUser() {
+  userSignUp(
+    'User',
+    'Demo',
+    'demouser',
+    'demouser@email.com',
+    'icyHam$ter48',
+  );
+}
+
+export {
+  userSignUp, userLogin, isNewUser, createDemoUser,
+};
